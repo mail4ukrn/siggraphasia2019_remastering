02949a0752573afc822359297789accfbde88926
mail4ukrn <145575063+mail4ukrn@users.noreply.github.com>
2023-10-30 02:11:10 +0530

    Update_download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 FILENAME="remasternet.pth.tar"
-FILEURL="http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar"
+FILEURL="https://file.io/DxmrKpg24k5A"
+FILEMD5="1219f5830e4a7208b1c7ba2f089a16c8"
 
 echo "Downloading the RemasterNet (245MB)..."
 wget --continue -O "model/$FILENAME" -- "$FILEURL"
 
-echo "Download finished successfully!"
+echo "Checking integrity (md5sum)..."
+OS=`uname -s`
+if [ "$OS" = "Darwin" ]; then
+  CHECKSUM=`cat model/$FILENAME | md5`
+else
+  CHECKSUM=`md5sum model/$FILENAME | awk '{ print $1 }'`
+fi
+
+if [ "$CHECKSUM" != "$FILEMD5" ]; then
+  echo "Integrity check failed. File is corrupt!"
+  echo "Try running this script again and if it fails remove 'models/$FILENAME' before trying again."
+  exit 1
+fi 
+echo -e "Download finished successfully!"
