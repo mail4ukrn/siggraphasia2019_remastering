60e54b7e05102ad1e01e8ba0308eddd139af06b1
mail4ukrn <145575063+mail4ukrn@users.noreply.github.com>
2023-10-30 03:47:56 +0530

    Update new_download_model.sh

--- new_download_model.sh
+++ new_download_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILENAME="remasternet.pth.tar"
-FILEURL="http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar"
+FILEURL="https://drive.google.com/file/d/1HYDsWF2pi3MrOikHqDm8da0cOVIxe8kT/view?usp=drive_link"
 
 echo "Downloading the RemasterNet (245MB)..."
 wget --continue -O "model/$FILENAME" -- "$FILEURL"
