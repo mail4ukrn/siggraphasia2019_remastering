e298fbf3fe11b4f7238bff88f36f47000d092dfe
mail4ukrn <145575063+mail4ukrn@users.noreply.github.com>
2023-10-30 01:48:10 +0530

    download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# md5sum 1219f5830e4a7208b1c7ba2f089a16c8
-
 FILENAME="remasternet.pth.tar"
 FILEURL="http://iizuka.cs.tsukuba.ac.jp/data/remasternet.pth.tar"
-FILEMD5="1219f5830e4a7208b1c7ba2f089a16c8"
 
 echo "Downloading the RemasterNet (245MB)..."
 wget --continue -O "model/$FILENAME" -- "$FILEURL"
 
-
+echo "Download finished successfully!"
